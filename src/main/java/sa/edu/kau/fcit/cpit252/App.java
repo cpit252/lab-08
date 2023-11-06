8e38d7f90256f0c8b75190291414fcaf65f41373
NawafIT <nwef918@gmail.com>
2023-11-06 16:51:52 +0300

    handle utf8 text

--- src/main/java/sa/edu/kau/fcit/cpit252/App.java
+++ src/main/java/sa/edu/kau/fcit/cpit252/App.java
@@ -2,9 +2,12 @@ package sa.edu.kau.fcit.cpit252;
 
 
 import java.io.File;
+import java.io.UnsupportedEncodingException;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URL;
+import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
 import java.util.List;
 
@@ -15,8 +18,11 @@ public class App {
      * Return the paths to the images in src/main/resources
      * @return an array that contains the absolute path to images in src/main/resources
      */
-    private static String[] getImages(){
+    private static String[] getImages() throws UnsupportedEncodingException {
         File directory = new File(App.class.getClassLoader().getResource("images").getPath());
+        String decodedPath = URLDecoder.decode(directory.getPath(), StandardCharsets.UTF_8.name());
+        System.out.println(decodedPath);
+        File directory2 = new File(decodedPath);
         File[] files = directory.listFiles();
         String []images = new String[files.length];
         for(int i=0; i<images.length; i++){
@@ -24,7 +30,7 @@ public class App {
         }
         return images;
     }
-    public static void main(String[] args) {
+    public static void main(String[] args) throws UnsupportedEncodingException {
         String dataPathOnMacBrew = "/opt/homebrew/Cellar/tesseract/5.3.3/share/tessdata";
         String dataPathOnMacPorts = "/opt/local/share/tessdata";
         String dataPathOnWindows = "C:\\Program Files\\Tesseract-OCR\\tessdata";
